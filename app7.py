ed02531f22bb5371919b087b8789f0c2f0cdf928
AKILA BANDARA <akilaprabash96@gmail.com>
2025-06-16 13:52:48 +0530

    Update app7.py

--- app7.py
+++ app7.py
@@ -7,19 +7,17 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/speedtest", methods=["GET"])
+@app.route("/speedtest")
 def run_speed_test():
     try:
         st = speedtest.Speedtest()
         st.get_best_server()
         download = st.download() / 1_000_000
         upload = st.upload() / 1_000_000
-
         return jsonify({
             "download_mbps": round(download, 2),
             "upload_mbps": round(upload, 2),
         })
-
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
