1ace91e8b48ab1934c4d513572c1e08ae420e3b6
AKILA BANDARA <akilaprabash96@gmail.com>
2025-06-16 13:46:14 +0530

    Update app7.py

--- app7.py
+++ app7.py
@@ -1,15 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import speedtest
 
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 @app.route("/speedtest", methods=["GET"])
 def run_speed_test():
     try:
         st = speedtest.Speedtest()
         st.get_best_server()
-
-        download = st.download() / 1_000_000  # Convert to Mbps
+        download = st.download() / 1_000_000
         upload = st.upload() / 1_000_000
 
         return jsonify({
@@ -19,10 +22,6 @@ def run_speed_test():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
-@app.route("/", methods=["GET"])
-def health():
-    return "Speedtest service is up!", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
